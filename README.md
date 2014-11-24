a783ebd70bfe239291f2b50c8cd31febecbd8da2
Arnas Udovicius <arnas.u@estina.lt>
2014-11-24 16:35:22 +0200

    main process; documentation

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 py-mail-command
 ===============
 
-script to read mails and execute commands by instructions. This should be run as cron job.
+Script to read mails and execute commands by instructions. This should be run as cron job.
+
+**Usage**
+
+Script reads mail form mailbox. It would be good to make separate mailbox per host. Mail body should contain host, command, arguments. Ex.:
+
+    HOME dir
+
+**Configuration**
+
+Config should be saved in ~/.config/mail_command.conf
+Example:
+
+    [mail]
+    server: mail.somewhere.lt
+    ssl: 1
+    user: username
+    password: super_druper_password
+    
+    [commands]
+    host: HOME
+    dir: ls -m
+    proc: ps ax
---
+++ mail_command.py
@@ -0,0 +1,44 @@
+import configparser
+import poplib
+from email import parser
+from os.path import expanduser
+from subprocess import check_output
+
+
+# prepare config for reading
+config = configparser.ConfigParser()
+config.read(expanduser("~") + '/.config/mail_command.conf')
+
+# connect to mail server
+if config.getboolean('mail', 'ssl'):
+    pop_conn = poplib.POP3_SSL(config.get('mail', 'server'))
+else:
+    pop_conn = poplib.POP3(config.get('mail', 'server'))
+
+pop_conn.user(config.get('mail', 'user'))
+pop_conn.pass_(config.get('mail', 'password'))
+
+# get messages from server
+messages = [pop_conn.retr(i) for i in range(1, len(pop_conn.list()[1]) + 1)]
+# concat message pieces
+messages = [b"\n".join(mssg[1]) for mssg in messages]
+# parse message intom an email object
+messages = [parser.BytesParser().parsebytes(mssg) for mssg in messages]
+for message in messages:
+    body = message.get_payload().split("\n")
+    for row in body:
+        e = row.split(' ')
+        
+        # check is host is same as in config
+        if e[0] != config.get('commands', 'host'):
+            continue
+
+        try:
+            command = config.get('commands', e[1]).split(' ')
+            out = check_output(command)
+            print(out)
+        except:
+            continue
+
+# close mail
+pop_conn.quit()
